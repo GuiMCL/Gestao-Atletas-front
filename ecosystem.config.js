ea7c33e8f2652bd6b134794455c9f6e72d712fef
guilherme.almeida <stanly1233254@gmail.com>
2026-02-05 11:30:38 -0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+dule.exports = {
   apps: [
     {
-      name: "financefull-dev",
-      cwd: "/FinanceFull",
+      name: "gestao-atletas-front-dev",
+      cwd: "/root/Gestao-Atletas-front", // ⚠️ pasta onde está o package.json
       script: "node_modules/next/dist/bin/next",
       args: "dev -p 3000",
       exec_mode: "fork",
